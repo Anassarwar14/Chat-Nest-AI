ff6f72630f1c34ce6276d55bfa63fb73f5817e1b
Anas Sarwar <anassarwar14@gmail.com>
2025-01-07 03:36:21 +0500

    created memory manager class integrated pinecone, upstash(redis, ratelimit), langchain, openai

---
+++ lib/memory.ts
@@ -0,0 +1,99 @@
+import { Redis } from "@upstash/redis";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Pinecone as PineconeClient } from '@pinecone-database/pinecone';
+import { PineconeStore } from "@langchain/pinecone";
+
+export type CharacterKey = {
+    characterName: string;
+    modelName: string;
+    userId: string;
+};
+
+export class MemoryManager {
+    private static instance: MemoryManager
+    private history: Redis;
+    private vectorDBClient: PineconeClient;
+
+    public constructor(){
+        this.history = Redis.fromEnv();
+        this.vectorDBClient = new PineconeClient({apiKey: process.env.PINECONE_API_KEY!});
+    }
+
+    public async vectorSearch(recentChatHistory: string, characterFileName: string) {
+        const pineconeClient = <PineconeClient>this.vectorDBClient;
+        const pineconeIndex = pineconeClient.Index(process.env.PINECONE_INDEX!);
+
+        const vectorStore = await PineconeStore.fromExistingIndex(
+            new OpenAIEmbeddings({openAIApiKey: process.env.OPENAI_API_KEY!}),
+            { pineconeIndex }
+        );
+
+        const similarDocs = await vectorStore
+            .similaritySearch(recentChatHistory, 3, { fileName: characterFileName })
+            .catch((err) => console.log("Failed to get vector search results", err));
+
+        return similarDocs;
+    }
+
+    //Singleton (SDA) - lazy initialization
+    public static async getInstance(): Promise<MemoryManager> {
+        if(!MemoryManager.instance){
+            MemoryManager.instance = new MemoryManager();
+        }
+
+        return MemoryManager.instance;
+    }
+
+    private generatedRedisCharacterKey(characterKey: CharacterKey): string {
+        return `${characterKey.characterName}-${characterKey.modelName}-${characterKey.userId}`
+    }
+
+    public async writeToHistory(text: string, characterKey: CharacterKey){
+        if(!characterKey || typeof characterKey.userId == 'undefined'){
+            console.log("Character key set incorrectly");
+            return "";
+        }
+
+        const key = this.generatedRedisCharacterKey(characterKey);
+        const result = await this.history.zadd(key, {
+            score: Date.now(),
+            member: text,
+        });
+
+        return result;
+    }
+
+    public async readLatestHistory(characterKey: CharacterKey): Promise<string> {
+        if(!characterKey || typeof characterKey.userId == 'undefined'){
+            console.log("Character key set incorrectly");
+            return "";
+        }
+
+        const key = this.generatedRedisCharacterKey(characterKey);
+        let result = await this.history.zrange(key, 0, Date.now(), {
+            byScore: true,
+        });
+
+        result = result.slice(-30).reverse();
+        const recentChats = result.reverse().join("\n");
+        return recentChats;
+    }
+    
+    public async seedChatHistory(seedContent: String, delimiter: string = "\n", characterKey: CharacterKey) {
+        const key = this.generatedRedisCharacterKey(characterKey);
+
+        if(await this.history.exists(key)){
+            console.log("User already has chat history");
+            return;
+        }
+
+        const content = seedContent.split(delimiter);
+        let counter = 0;
+        for(const line of content){
+            await this.history.zadd(key, { score: counter, member: line });
+            counter +=1 ;
+        }
+    }
+
+
+}
